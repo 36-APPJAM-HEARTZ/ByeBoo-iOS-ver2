a18f36ffad5ca58f8a0daa6645c5053d6c3aa0fc
juri123123 <jrjr4426@gmail.com>
2026-06-15 17:28:54 +0900

    mig: #7 - interceptor, base response

--- Projects/Data/Sources/Network/Services/NetworkService.swift
+++ Projects/Data/Sources/Network/Services/NetworkService.swift
@@ -11,6 +11,8 @@ import Foundation
 import Alamofire
 import KakaoSDKUser
 
+import Core
+
 protocol NetworkService {
     func request<T: Decodable>(
         _ endPoint: EndPoint,
@@ -250,67 +252,67 @@ extension DefaultNetworkService: ASAuthorizationControllerDelegate, ASAuthorizat
     }
 }
 
-final class MockNetworkService: NetworkService {
-    
-    private(set) var kakaoRequestCalled = false
-    private(set) var appleRequestCalled = false
-    private let userAPI: UserAPIManager
-    
-    init(userAPI: UserAPIManager) {
-        self.userAPI = userAPI
-    }
-    
-    func request<T>(_ endPoint: any EndPoint, decodingType: T.Type) throws -> T where T : Decodable {
-        let dummyJSON = "{}".data(using: .utf8)!
-        return try JSONDecoder().decode(T.self, from: dummyJSON)
-    }
-    
-    func request(_ endPoint: any EndPoint) throws {
-        
-    }
-    
-    func request(image: Data, signedURL: String) throws {
-        
-    }
-    
-    func kakaoRequest() throws -> String {
-        kakaoRequestCalled = true
-        
-        var idToken: String?
-        
-        if userAPI.isKakaoTalkLoginAvailable() {
-            userAPI.loginWithKakaoTalk { oauthToken, error in
-                idToken = oauthToken?.accessToken
-            }
-        } else {
-            userAPI.loginWithKakaoAccount { oauthToken, error in
-                idToken = oauthToken?.accessToken
-            }
-        }
-        
-        return idToken ?? ""
-    }
-    
-    func appleRequest() throws -> (String, String) {
-        appleRequestCalled = true
-        
-        let identityToken = AppleToken.stub().identityToken
-        let authorizationCode = AppleToken.stub().authorizationCode
-       
-        return (identityToken, authorizationCode)
-    }
-}
-
-struct AppleToken {
-    let identityToken: String
-    let authorizationCode: String
-}
-
-extension AppleToken {
-    static func stub() -> AppleToken {
-        .init(
-            identityToken: "identityToken",
-            authorizationCode: "authorizationCode"
-        )
-    }
-}
+//final class MockNetworkService: NetworkService {
+//    
+//    private(set) var kakaoRequestCalled = false
+//    private(set) var appleRequestCalled = false
+//    private let userAPI: UserAPIManager
+//    
+//    init(userAPI: UserAPIManager) {
+//        self.userAPI = userAPI
+//    }
+//    
+//    func request<T>(_ endPoint: any EndPoint, decodingType: T.Type) throws -> T where T : Decodable {
+//        let dummyJSON = "{}".data(using: .utf8)!
+//        return try JSONDecoder().decode(T.self, from: dummyJSON)
+//    }
+//    
+//    func request(_ endPoint: any EndPoint) throws {
+//        
+//    }
+//    
+//    func request(image: Data, signedURL: String) throws {
+//        
+//    }
+//    
+//    func kakaoRequest() throws -> String {
+//        kakaoRequestCalled = true
+//        
+//        var idToken: String?
+//        
+//        if userAPI.isKakaoTalkLoginAvailable() {
+//            userAPI.loginWithKakaoTalk { oauthToken, error in
+//                idToken = oauthToken?.accessToken
+//            }
+//        } else {
+//            userAPI.loginWithKakaoAccount { oauthToken, error in
+//                idToken = oauthToken?.accessToken
+//            }
+//        }
+//        
+//        return idToken ?? ""
+//    }
+//    
+//    func appleRequest() throws -> (String, String) {
+//        appleRequestCalled = true
+//        
+//        let identityToken = AppleToken.stub().identityToken
+//        let authorizationCode = AppleToken.stub().authorizationCode
+//       
+//        return (identityToken, authorizationCode)
+//    }
+//}
+//
+//struct AppleToken {
+//    let identityToken: String
+//    let authorizationCode: String
+//}
+//
+//extension AppleToken {
+//    static func stub() -> AppleToken {
+//        .init(
+//            identityToken: "identityToken",
+//            authorizationCode: "authorizationCode"
+//        )
+//    }
+//}
