4f6c788f8324e8ea24af8f42a8081d527143da24
juri123123 <jrjr4426@gmail.com>
2026-06-16 15:34:45 +0900

    feat: #18 - presentationKit DocC 도입

--- Projects/PresentationKit/Sources/Protocols/MixpanelProperty.swift
+++ Projects/PresentationKit/Sources/Protocols/MixpanelProperty.swift
@@ -7,7 +7,11 @@
 
 import Mixpanel
 
+/// Mixpanel 분석 도구에 전달할 속성을 정의하는 프로토콜입니다.
+/// 분석용 데이터를 딕셔너리 형태로 변환할 수 있게 합니다.
 public protocol MixpanelProperty {
+    /// Mixpanel 서버로 전송할 속성 딕셔너리입니다.
     var dictionary: [String: MixpanelType] { get }
 }
+
 
